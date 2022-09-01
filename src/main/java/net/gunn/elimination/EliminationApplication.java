5d6207cb16ad3f9e1f35f1e54535abf8b8a214ad
Alec Petridis <chop@quenda.net>
2022-08-31 17:46:01 -0700

    removed (and invalided) credentials because i acciedntally committed them

--- src/main/java/net/gunn/elimination/EliminationApplication.java
+++ src/main/java/net/gunn/elimination/EliminationApplication.java
@@ -2,6 +2,7 @@ package net.gunn.elimination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class EliminationApplication {
--- src/main/java/net/gunn/elimination/auth/WebSecurityConfig.java
+++ src/main/java/net/gunn/elimination/auth/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package net.gunn.elimination.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,8 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +30,9 @@ class WebSecurityConfig {
     }
 
     @Bean
-    protected SecurityFilterChain webSecurityCustomizer(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain webSecurityCustomizer(
+        HttpSecurity http
+        ) throws Exception {
         return http
             .authorizeRequests()
             .anyRequest()
@@ -32,24 +40,25 @@ class WebSecurityConfig {
             .and()
             .oauth2Login()
             .userInfoEndpoint(n -> n.oidcUserService(userDetailsService))
-            .successHandler(new SimpleUrlAuthenticationSuccessHandler() {{ setUseReferer(true); }})
+            .successHandler(new SimpleUrlAuthenticationSuccessHandler() {{
+                setUseReferer(true);
+            }})
             .and()
             .build();
     }
 
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(
-        @Value("${spring.security.oauth2.client.registration.google.clientId}") String clientId,
-        @Value("${spring.security.oauth2.client.registration.google.clientSecret}") String clientSecret
-    ) {
+    public ClientRegistrationRepository clientRegistrationRepository(@Value("classpath:client_secret.json") Resource clientSecret, ObjectMapper mapper) throws IOException {
+        var config = mapper.readTree(clientSecret.getInputStream()).get("web");
         return new InMemoryClientRegistrationRepository(
             CommonOAuth2Provider
                 .GOOGLE
                 .getBuilder("google")
                 .redirectUri("{baseUrl}/login/oauth2/code/google")
+                .scope("email", "profile")
                 .userNameAttributeName(IdTokenClaimNames.SUB)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
+                .clientId(config.get("client_id").asText())
+                .clientSecret(config.get("client_secret").asText())
                 .build()
         );
     }

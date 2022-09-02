0d6098891fe32da1876c5fe0d40125a67f06f00f
Alec Petridis <chop@quenda.net>
2022-09-01 19:35:11 -0700

    lol??

--- src/main/java/net/gunn/elimination/repository/RoleRepository.java
+++ src/main/java/net/gunn/elimination/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package net.gunn.elimination.repository;
 
 import net.gunn.elimination.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Component
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 
